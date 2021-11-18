acee6cb3a48da7cf352f7fba64f35aa53eb25244
ramcharan208 <76614727+ramcharan208@users.noreply.github.com>
2021-11-18 17:20:56 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "WINDOWS C";
+const accessCode2 = "CODING";
+const accessCode3 = "VIRUS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("WDIWSCNO", 100,50)
+    fill("lightblue")
+    text("Hint: windows Program drive name  !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("ONCGID", 700,150)
+    fill("lightblue")
+    text("Hint: Instruction of computer!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("RVSIU", 100,250)
+    fill("lightblue")
+    text("Hint: If it come system will die !!", 100,270)
+
+}
